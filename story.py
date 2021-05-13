161cfbb27b7ec149187198615f39e9448dc744a0
FlyN-Nick <43420132+FlyN-Nick@users.noreply.github.com>
2021-05-13 12:19:46 -0700

    Finished the story.

--- story.py
+++ story.py
@@ -7,7 +7,15 @@ class MomentsEnum(Enum):
     BEG_CONT = 3
     WEARING_CROWN = 4
     WEIGHING_METALS = 5
-    BATHTUP = 6
+    BATHTUB = 6
+    BATHTUB_CONT = 7
+    EUREKA = 8
+    NAKED = 9
+    BOTH = 10
+    REAL = 11
+    NOT_READY = 12
+    REAL_CONT = 13
+    EXIT = 14
 
 moments_dict = {
     MomentsEnum.INTRO: {
@@ -16,52 +24,67 @@ moments_dict = {
         "choices": [MomentsEnum.BEG, MomentsEnum.BEG]
     },
     MomentsEnum.BEG: {
-        "text": "\nKing Hiero II of Syracuse had a golden crown made for himself.\nHe gave a local goldsmith an adequate amount of gold to produce the crown,\nbut suspected that the crown made was impure,\nwith the goldsmith taking some of the gold for himself.\n",
+        "text": "\nKing Hiero II of Syracuse had a golden crown made for himself.\nHe gave a local goldsmith an adequate amount of gold to produce the crown,\nbut suspected that the crown made was impure with silver,\nwith the goldsmith taking some of the gold for himself.\n",
         "prompt": "1: continue.\n\n",
         "choices": [MomentsEnum.BEG_CONT]
     },
     MomentsEnum.BEG_CONT: {
         "text": "\nFollowing his suspicion, he tasked Archimedes with testing the purity of his crown.\nArchimedes had to answer a question,\nthe question of how he would accomplish this.\n",
         "prompt": "When do you think he came up with an answer?\n1: when wearing the King's crown.\n2: when wearing some gold and silver.\n3: in a bathtub.\n\n",
-        "choices": [MomentsEnum.INTRO, MomentsEnum.INTRO, MomentsEnum.INTRO]
+        "choices": [MomentsEnum.WEARING_CROWN, MomentsEnum.WEIGHING_METALS, MomentsEnum.BATHTUB]
+    },
+    MomentsEnum.WEARING_CROWN: {
+        "text": "\nUnfortunately, that's not how the story goes.\n",
+        "prompt": "1: back.\n\n",
+        "choices": [MomentsEnum.BEG_CONT]
+    },
+    MomentsEnum.WEIGHING_METALS: {
+        "text": "\nWhile gold and silver have different densities (a key fact for later), that's not how the story goes.\n",
+        "prompt": "1: back.\n\n",
+        "choices": [MomentsEnum.BEG_CONT]
+    },
+    MomentsEnum.BATHTUB: {
+        "text": "\nWhen in a bath, Archimedes realized that his body displaced water in an amount equal to his volume.\nTherefore, water could be used to measure the volume of non-geometric objects.\n",
+        "prompt": "1: continue.\n\n",
+        "choices": [MomentsEnum.BATHTUB_CONT]
+    },
+    MomentsEnum.BATHTUB_CONT: {
+        "text": "\nWith water, therefore, the volume of the crown could be measured.\nThen, if the crown was weighed, its density could be calculated.\nThe density of the crown could then be compared to that of pure gold, as silver is less dense than gold.\n",
+        "prompt": "What do you think happened when he made this realization?\n1: he exclaimed \"Eureka!\" out of joy.\n2: he leapt out of his bath and ran through the streets of Syracuse naked.\n3: he did all of the above.\n\n",
+        "choices": [MomentsEnum.EUREKA, MomentsEnum.NAKED, MomentsEnum.BOTH]
+    },
+    MomentsEnum.EUREKA: {
+        "text": "\nWhile he did shout \"Eureka!\", twice, he also did so while running through the streets of Syracuse naked.\nWell, at least that's how the story goes.\nThe true story isn't as much of a spectacle.\n",
+        "prompt": "Ready to learn about the real story?\n1: yes.\n2: also yes.\n3: no.\n\n",
+        "choices": [MomentsEnum.REAL, MomentsEnum.REAL, MomentsEnum.NOT_READY]
+    },
+    MomentsEnum.NAKED: {
+        "text": "\nWhile he did run through the streets of Syracuse naked, he also did so while shouting \"Eureka!\" twice.\nWell, at least that's how the story goes.\nThe true story isn't as much of a spectacle.\n",
+        "prompt": "Ready to learn about the real story?\n1: yes.\n2: also yes.\n3: no.\n\n",
+        "choices": [MomentsEnum.REAL, MomentsEnum.REAL, MomentsEnum.NOT_READY]
+    },
+    MomentsEnum.BOTH: {
+        "text": "\nArchimedes indeed shouted \"Eureka!\" twice as he leapt out of his bath and ran through the streets of Syracuse naked.\nWell, at least that's how the story goes.\nThe true story isn't as much of a spectacle.\n",
+        "prompt": "Ready to learn about the real story?\n1: yes.\n2: also yes.\n3: no.\n\n",
+        "choices": [MomentsEnum.REAL, MomentsEnum.REAL, MomentsEnum.NOT_READY]
+    },
+    MomentsEnum.REAL: {
+        "text": "\nAlthough no one can know for sure, Archimedes probably solved the king's demand with \"Archimedes' principle\".\nThe principle, also known as the law of buoyancy,\nstates that the upward buoyant force exerted on a body immersed in a fluid\nis equal to the weight of the fuild that the body displaces.\nThis is modeled with the equation F = -pgV,\nwhere F is the buoyant force,\np is the density of the fluid,\ng is the accelaration due to gravity,\nand V is the volume of fluid displaced.\n",
+        "prompt": "Ready for further explanation?\n1: yes.\n\n",
+        "choices": [MomentsEnum.REAL_CONT]
+    },
+    MomentsEnum.NOT_READY: {
+        "text": "\nSorry, you are going to learn about it anyways, it's not your choice.\n",
+        "prompt": "1: continue.\n\n",
+        "choices": [MomentsEnum.REAL]
+    },
+    MomentsEnum.REAL_CONT: {
+        "text": "\nInstead of following the method previously mentioned,\nArchimedes probably instead used a contraption (one that is difficult to explain without a diagram) that followed his principle to determine the purity of the crown.\nThis principle of his is crucial to the field of hydrostatics,\nand his discovery of it is one of the reasons why he is one of the most famous mathematicians of ancient Greece.\nAlthough the story of him running through the streets naked is probably fantasy, we can hope that he discovered the principle in a bath.\n",
+        "prompt": "1: home.\n\n",
+        "choices": [MomentsEnum.INTRO]
     }
 }
 
-"""
-INTRO:
-Welcome to my story of science! 
-You will use the number keys to make choices as you learn about Archimedes, 
-the most famous mathematician of ancient Greece.
-
-0: start.
-
-BEGINNING: 
-King Hiero II of Syracuse had a golden crown made for himself. 
-He gave a local goldsmith an adequate amount of gold to produce the crown, 
-but suspected that the crown made was impure, 
-with the goldsmith taking some of the gold for himself.
-0: continue.
-
-BEGINNING (cont):
-Following his suspicion, he tasked Archimedes with testing the purity of his crown.
-Archimedes had to answer a question,
-the question of how he would accomplish this.
-
-When do you think he came up with an answer?
-0: when wearing the King's crown.
-1: when weighing some gold and silver.
-2: in a bathtub.
-
-Wearing the crown:
-Unfortunately, that's not how the story goes.
-
-0: back.
-
-Weighing gold and silver:
-
-
-
-"""
 class Moment:
     def __init__(self, moment_name):
         self.text = moments_dict[moment_name]['text']
